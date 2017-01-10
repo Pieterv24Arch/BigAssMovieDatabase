bc07ebf7c39af56a1355bef4a6d7ccadd1991b64
Lex Punter <lex.punter@gmail.com>
2017-01-10 11:20:42 +0100

    created Genre and Runtime parser

--- BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/DataType.java
+++ BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/DataType.java
@@ -1,19 +1,28 @@
 package net.pieterdev.BigAssMovieParser;
 
 import net.pieterdev.BigAssMovieParser.Parsers.MovieParser;
+import net.pieterdev.BigAssMovieParser.Parsers.RuntimeParser;
+import net.pieterdev.BigAssMovieParser.Parsers.GenreParser;
 import net.pieterdev.BigAssMovieParser.Parsers.ParserBase;
 
 public enum DataType {
-    MOVIES;
+    MOVIES,
+    GERNES,
+    RUNTIMES;
 
     MovieParser movieParser = new MovieParser();
-
+    GenreParser genreParser = new GenreParser();
+    RuntimeParser runtimeParser = new RuntimeParser();
     public ParserBase getParser()
     {
         switch (this)
         {
             case MOVIES:
                 return movieParser;
+            case GERNES:
+                return genreParser;
+            case RUNTIMES:
+                return runtimeParser;
             default:
                 return null;
         }
