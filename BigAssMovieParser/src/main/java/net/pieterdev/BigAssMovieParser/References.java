b590b1e667c0c2484d8ffc6a2f000a0ad93a0bbf
dijk1526 <dijk1526@student.nhl.nlgit config --global user.name dijk1526git config --global user.email dijk1526@student.nhl.nl>
2017-01-10 14:18:10 +0100

    Added Rating and country parser/loader

--- BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/References.java
+++
@@ -1,6 +0,0 @@
-package net.pieterdev.BigAssMovieParser;
-
-public class References {
-    public static String MOVIES_PATH = "/home/pieter/Documents/Movies.txt";
-    public static String SERIES_PATH = "/home/pieter/Documents/Series.txt";
-}
