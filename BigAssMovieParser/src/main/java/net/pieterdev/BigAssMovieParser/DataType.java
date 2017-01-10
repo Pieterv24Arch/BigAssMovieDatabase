6f5ca7369327bd6ce9eb8ae9e6ac4a0ea83c3210
dijk1526 <dijk1526@student.nhl.nlgit config --global user.name dijk1526git config --global user.email dijk1526@student.nhl.nl>
2017-01-10 14:20:23 +0100

    Added Country/Rating parser/loader

--- BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/DataType.java
+++ BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/DataType.java
@@ -1,21 +1,27 @@
 package net.pieterdev.BigAssMovieParser;
 
 import net.pieterdev.BigAssMovieParser.Parsers.ActorParser;
+import net.pieterdev.BigAssMovieParser.Parsers.CountryParser;
 import net.pieterdev.BigAssMovieParser.Parsers.MPAAParser;
 import net.pieterdev.BigAssMovieParser.Parsers.MovieParser;
 import net.pieterdev.BigAssMovieParser.Parsers.ParserBase;
+import net.pieterdev.BigAssMovieParser.Parsers.RatingParser;
 import net.pieterdev.BigAssMovieParser.Parsers.WriterParser;
 
 public enum DataType {
     MOVIES,
-    ACTORS;
+    ACTORS,
     WRITERS,
-    MPAA;
+    MPAA, 
+    COUNTRIES,
+    RATINGS;
 
     MovieParser movieParser = new MovieParser();
     WriterParser writerParser = new WriterParser();
     MPAAParser mpaaParser = new MPAAParser();
     ActorParser actorParser = new ActorParser();
+    CountryParser countriesParser = new CountryParser();
+    RatingParser ratingsParser = new RatingParser();
 
     public ParserBase getParser()
     {
@@ -29,6 +35,10 @@ public enum DataType {
                 return writerParser;
             case MPAA:
                 return mpaaParser;
+            case COUNTRIES:
+                return countriesParser;
+            case RATINGS:
+                return ratingsParser;
             default:
                 return null;
         }
