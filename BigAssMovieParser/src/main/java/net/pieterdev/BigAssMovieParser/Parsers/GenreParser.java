14176c7acc401e5477ec7a34b509ba74087e024d
Lex Punter <lex.punter@gmail.com>
2017-01-11 11:59:08 +0100

    added comments

--- BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/Parsers/GenreParser.java
+++ BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/Parsers/GenreParser.java
@@ -2,8 +2,17 @@ package net.pieterdev.BigAssMovieParser.Parsers;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/**
+ * Parser for the movie genres
+ * @author lexpunter
+ */
 public class GenreParser implements ParserBase{
+        /**
+        * Parser logic is used on given line.
+         * If line does not conform with patterns an empty string is returned.
+         * @param line line to be parsed
+         * @return parsed string or and empty string
+        */
     public String parseString(String line)
     {
         Pattern genrePattern = getPattern();
@@ -12,7 +21,14 @@ public class GenreParser implements ParserBase{
         System.out.println(line);
         if(m.find())
         {
-            
+            /**
+             * Group 1: Movie/Serie name
+             * Group 2: MovieYear
+             * Group 3: Episode name, null if not found
+             * Group 4: Season number, null if not found
+             * Group 5: Episode number, null if not found
+             * Group 6: Genre
+             */
             if(line.startsWith("\""))
             {
                 return String.format("%s~%s~%s~%s~%s~%s", m.group(1).trim(), m.group(2), m.group(3).trim(), m.group(4), m.group(5), m.group(6));
--- BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/Parsers/RuntimeParser.java
+++ BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/Parsers/RuntimeParser.java
@@ -3,22 +3,30 @@ package net.pieterdev.BigAssMovieParser.Parsers;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Parser for the running-times
+ * @author lexpunter
+ */
 public class RuntimeParser implements ParserBase{
+        /**
+         * Parser logic is used on given line.
+         * If line does not conform with patterns an empty string is returned.
+         * @param line line to be parsed
+         * @return parsed string or and empty string
+         */
     public String parseString(String line)
     {
         Pattern genrePattern = getPattern();
-       
         Matcher m = genrePattern.matcher(line);
-        System.out.println(line);
         if(m.find())
         {
             /**
              * Group 1: Movie/Serie name
              * Group 2: MovieYear
-             * Group 3: Episode name
-             * Group 4: Season number
-             * Group 5: Episode number
-             * Group 6: Genre
+             * Group 3: Episode name, null if not found
+             * Group 4: Season number, null if not found
+             * Group 5: Episode number, null if not found
+             * Group 6: Running-time
              */
             if(line.startsWith("\""))
             {
