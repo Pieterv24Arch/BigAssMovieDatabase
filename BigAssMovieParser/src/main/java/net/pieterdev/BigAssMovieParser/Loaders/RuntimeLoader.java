4bfca16dc7f4d3d2d9145aa7fa25318178dc3003
Lex Punter <lex.punter@gmail.com>
2017-01-11 11:57:53 +0100

    added comments

---
+++ BigAssMovieParser/src/main/java/net/pieterdev/BigAssMovieParser/Loaders/RuntimeLoader.java
@@ -0,0 +1,26 @@
+package net.pieterdev.BigAssMovieParser.Loaders;
+
+import net.pieterdev.BigAssMovieParser.DataType;
+import net.pieterdev.BigAssMovieParser.Parsers.ParserBase;
+import net.pieterdev.BigAssMovieParser.References;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * Loader for the genre list.
+ * this class loads the targeted file, sends lines to the parser and then writes those lines back to new files
+ * @author Lex
+ */
+public class RuntimeLoader extends BaseLoader {
+     /**
+     * Declaration of target file and file type.
+     */
+    public RuntimeLoader() {
+        super("runtimeTest.txt", DataType.RUNTIMES);
+    }
+}
+    
